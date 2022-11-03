82b2386f8af068e8ffedd860adf0fc96c5e21794
ryuda <ryuda0101@naver.com>
2022-11-03 12:23:04 +0900

    작업 완료

--- public/js/header_gnb.js
+++ public/js/header_gnb.js
@@ -1,18 +1,27 @@
 // gnb 메뉴 관련 클릭 이벤트
 let roomStart = document.querySelector("#main_page .rooms").offsetTop;
 let facilitiesStart = document.querySelector("#main_page .facilities").offsetTop;
-let newsStart = document.querySelector("#main_page .news").offsetTop;
+// let newsStart = document.querySelector("#main_page .news").offsetTop;
 
-const rooms_gnb = document.querySelector("#header .bot_box .gnb .rooms_gnb")
+const rooms_gnb = document.querySelector("#header .bot_box .gnb .rooms")
+const rooms_footer_gnb = document.querySelector("#footer .footer_info .bot_box .rooms_gnb")
 const restaurant_gnb = document.querySelector("#header .bot_box .gnb .restaurant_gnb")
-const news_gnb = document.querySelector("#header .bot_box .gnb .news_gnb")
+const restaurant_footer_gnb = document.querySelector("#footer .footer_info .bot_box .restaurant_gnb")
+// const news_gnb = document.querySelector("#header .bot_box .gnb .news_gnb")
 
 const rooms_hamGnb = document.querySelector("#header .ham_gnb .rooms_gnb")
 const restaurant_hamGnb = document.querySelector("#header .ham_gnb .restaurant_gnb")
-const news_hamGnb = document.querySelector("#header .ham_gnb .news_gnb")
+// const news_hamGnb = document.querySelector("#header .ham_gnb .news_gnb")
 
 
 // 함수 호출및 클릭시 이벤트 걸어주기
+rooms_gnb.addEventListener("click",function(event){
+    roomWork();
+});
+
+rooms_footer_gnb.addEventListener("click",function(event){
+    roomWork();
+});
 rooms_hamGnb.addEventListener("click",function(event){
     roomWork();
 });
@@ -23,13 +32,16 @@ restaurant_gnb.addEventListener("click",function(event){
 restaurant_hamGnb.addEventListener("click",function(event){
     facilitiesWork();
 });
+restaurant_footer_gnb.addEventListener("click",function(event){
+    facilitiesWork();
+});
 
-news_gnb.addEventListener("click",function(event){
-    newsWork();
-});
-news_hamGnb.addEventListener("click",function(event){
-    newsWork();
-});
+// news_gnb.addEventListener("click",function(event){
+//     newsWork();
+// });
+// news_hamGnb.addEventListener("click",function(event){
+//     newsWork();
+// });
 
 // 함수 세팅
 function roomWork (){
@@ -48,10 +60,10 @@ function facilitiesWork (){
     });
 }
 
-function newsWork(){
-    event.preventDefault();
-    window.scrollTo({
-        top:newsStart,
-        behavior:"smooth"
-    });
-}
+// function newsWork(){
+//     event.preventDefault();
+//     window.scrollTo({
+//         top:newsStart,
+//         behavior:"smooth"
+//     });
+// }
